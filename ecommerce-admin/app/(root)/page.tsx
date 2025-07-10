a8339c1e854d2f7083fa3e8c7960dc7599f1a20a
Khanh Phan <khanhphanba0123@gmail.com>
2025-07-10 16:56:04 +0700

    Environment Setup

---
+++ ecommerce-admin/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+        <p>
+        Admin Dashboard
+        </p>
+        <Button variant="destructive">Go to Dashboard</Button>
+    </div>
+  );
+}
