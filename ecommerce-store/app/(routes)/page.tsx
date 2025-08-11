4e689ba16bf57d610bcd47d4d5c48a8e59228c73
Khanh Phan <khanhphanba0123@gmail.com>
2025-08-11 22:27:45 +0700

    Add featured products

---
+++ ecommerce-store/app/(routes)/page.tsx
@@ -0,0 +1,28 @@
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "../components/billboard";
+import Container from "../components/ui/container";
+import getProducts from "@/actions/get-products";
+import ProductList from "../components/product-list";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const products = await getProducts({ isFeatured: true });
+    const billboard = await getBillboard("40768d24-b2c2-4f2c-a971-1efff2105845");
+
+    return (
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard className="text-white" data={billboard} />
+                <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                    <ProductList
+                        title="Featured Products"
+                        items={products}
+                    />
+                </div>
+            </div>
+        </Container>
+    );
+}
+
+export default HomePage;
---
+++ ecommerce-store/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Urbanist } from "next/font/google";
+import "./globals.css";
+import Footer from "./components/footer";
+import Navbar from "./components/navbar";
+
+const font = Urbanist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+
+export const metadata: Metadata = {
+  title: "Store",
+  description: "Store",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${font.className} antialiased`}
+      >
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
