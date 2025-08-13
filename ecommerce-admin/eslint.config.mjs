f6741d07c2c37f738aac3dac431bd011396feb30
Khanh Phan <khanhphanba0123@gmail.com>
2025-08-13 10:27:31 +0700

    more rules disabled

--- ecommerce-admin/eslint.config.mjs
+++ ecommerce-admin/eslint.config.mjs
@@ -17,6 +17,8 @@ const eslintConfig = [
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-require-imports': 'off',
       '@typescript-eslint/no-unused-expressions': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-this-alias': 'off',
     },
   }),
 ];
